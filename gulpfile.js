461ce160113c03ec817a96f1087bb414d4ac253c
Pavel <alexeevpavel2014@gmail.com>
2018-04-20 12:43:36 +0300

    Updates
    - Install Gulp-mincss
    - Rename folder style to css
    - Add minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify');
+    uglifycss = require('gulp-cssmin');
 
-gulp.task('minify', function () {
+gulp.task('minifyjs', function () {
     gulp.src('js/registration.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('minifycss', function () {
+    gulp.src('css/style.js')
         .pipe(uglify())
         .pipe(gulp.dest('build'));
 });
