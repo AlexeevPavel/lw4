bb74ed86d949f6d00573d4f30800b53daa5755d9
Pavel <alexeevpavel2014@gmail.com>
2018-04-28 15:21:05 +0300

    Updates
    - fix inject
    - delet gulp-contrib-cop
    - fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,63 @@
-var gulp = require('gulp'),
+var gulp = require('gulp');
   uglify = require('gulp-uglify');
   uglifycss = require('gulp-cssmin');
-  copy = require('gulp-contrib-copy'); 
   rename = require('gulp-rename');
   browserSync = require('browser-sync');
   inject = require('gulp-inject');
-  watcher = require('gulp-watch')
+  watcher = require('gulp-watch');
   reload = browserSync.reload;
   path = {
-      html:['index.html'],
-      css:['css/style.css'],
-      js:['js/registration.js']
+    build: {
+      html: 'build/',
+      css: 'build/css/',
+      js: 'build/js/',
+      img: 'build/img/'
+    },
+    src: {
+      html: 'src/*.html',
+      js: 'src/js/registration.js',
+      jquery: 'src/js/jquery.min.js',
+      css: 'src/css/style.css',
+      img: 'src/img/*.*'
+    }
   };
-  
-gulp.task('html', function(){
-  gulp.src(paths.html)
-  .pipe(reload({stream:true}));
-});
-
-gulp.task('css', function(){
-  gulp.src(paths.css)
-  .pipe(reload({stream:true}));
-});
-
-gulp.task('js', function(){
-  gulp.src(paths.js)
-  .pipe(reload({stream:true}));
-});
 
 gulp.task('minifyjs', function () {
-  gulp.src('js/registration.js')
+  gulp.src(path.src.js)
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest(path.build.js))
+    .pipe(reload({stream: true}));
 });
 
 gulp.task('minifycss', function () {
-  gulp.src('css/style.css')
+  gulp.src(path.src.css)
     .pipe(uglifycss())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest(path.build.css))
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('copyhtml', function() {
-  gulp.src('index.html')
-    .pipe(copy())
-    .pipe(gulp.dest('build'));
+gulp.task('html', function() {
+  gulp.src(path.src.html)
+    .pipe(gulp.dest(path.build.html))
+  .on('end', function () {
+    gulp.src(path.build.html + 'index.html')
+    .pipe(inject(gulp.src('./build/js/*.js', {read: false}), {relative: true}))
+    .pipe(inject(gulp.src('./build/css/style.min.css', {read: false}), {relative: true}))
+    .pipe(gulp.dest(path.build.html));
+  })
+  .pipe(reload({stream: true}));
 });
 
 gulp.task('copyimg', function() {
-  gulp.src('img/background.jpg')
-    .pipe(copy())
-    .pipe(gulp.dest('build/img'));
+  gulp.src(path.src.img)
+    .pipe(gulp.dest(path.build.img));
 });
 
 gulp.task('copyjquery', function() {
-  gulp.src('js/jquery.min.js')
-    .pipe(copy())
-    .pipe(gulp.dest('build/js'));
+  gulp.src(path.src.jquery)
+    .pipe(gulp.dest(path.build.js))
 });
 
-gulp.task('inj',function () {
-  var target = gulp.src('./build/index.html')
-  var sources = gulp.src(['./build/**/*.js', './build/**/*.css'], {read:false});
-  return target.pipe(inject(sources))
-    .pipe(gulp.dest('./build'));
-})
-
-gulp.task('build', ['minifyjs','minifycss','copyimg','copyhtml','copyjquery','inj']); 
+gulp.task('build', ['html','minifyjs','copyjquery','minifycss','copyimg']); 
