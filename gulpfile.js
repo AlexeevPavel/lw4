10751ccb88cb1e58de9a9aa0cb4d09054bc75aa6
Pavel <alexeevpavel2014@gmail.com>
2018-04-28 15:27:27 +0300

    Updates
    - add dev
    - add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,24 @@ var gulp = require('gulp');
       jquery: 'src/js/jquery.min.js',
       css: 'src/css/style.css',
       img: 'src/img/*.*'
+    },
+    watch: {
+      html: 'src/**/*.html',
+      js: 'src/js/**/*.js',
+      style: 'src/css/**/*.css',
+      img: 'src/img/**/*.*'
     }
   };
+
+var config = {
+  server: {
+    baseDir: "./build"
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 8080,
+  logPrefix: "MrHunfer"
+};
 
 gulp.task('minifyjs', function () {
   gulp.src(path.src.js)
@@ -61,3 +77,29 @@ gulp.task('copyjquery', function() {
 });
 
 gulp.task('build', ['html','minifyjs','copyjquery','minifycss','copyimg']); 
+
+gulp.task('watch', function () {
+  watch([path.watch.html], function(event, cb) {
+    gulp.start('html:build');
+  });
+  watch([path.watch.style], function(event, cb) {
+    gulp.start('style:build');
+  });
+  watch([path.watch.js], function(event, cb) {
+    gulp.start('js:build');
+  });
+  watch([path.watch.img], function(event, cb) {
+    gulp.start('image:build');
+  });
+  watch([path.watch.fonts], function(event, cb) {
+    gulp.start('fonts:build');
+  });
+})
+
+gulp.task('webserver', function () {
+  browserSync(config);
+});
+
+gulp.task('dev', ['build', 'webserver', 'watch']);
+
+gulp.task('default', ['dev']);
