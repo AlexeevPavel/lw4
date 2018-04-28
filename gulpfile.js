446aa0852cd72aba782c9fa83582c49a1a19e983
Pavel <alexeevpavel2014@gmail.com>
2018-04-28 12:14:21 +0300

    Updates
    - install inject and watcher
    - add inject to index.html
    - fix rename for gilpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,41 @@ var gulp = require('gulp'),
   copy = require('gulp-contrib-copy'); 
   rename = require('gulp-rename');
   browserSync = require('browser-sync');
+  inject = require('gulp-inject');
+  watcher = require('gulp-watch')
   reload = browserSync.reload;
   path = {
       html:['index.html'],
       css:['css/style.css'],
-      script:['js/registration.js']
+      js:['js/registration.js']
   };
+  
+gulp.task('html', function(){
+  gulp.src(paths.html)
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('css', function(){
+  gulp.src(paths.css)
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('js', function(){
+  gulp.src(paths.js)
+  .pipe(reload({stream:true}));
+});
 
 gulp.task('minifyjs', function () {
   gulp.src('js/registration.js')
     .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('minifycss', function () {
   gulp.src('css/style.css')
     .pipe(uglifycss())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('build/css'));
 });
 
@@ -35,30 +54,17 @@ gulp.task('copyimg', function() {
     .pipe(gulp.dest('build/img'));
 });
 
-gulp.task('minifyall', function () {
-  gulp.src('js/registration.js')
-    .pipe(uglify())
-    .pipe(rename({
-      basename: "registration",
-      suffix: ".min",
-      extname: ".js"
-    }))
-    .pipe(gulp.dest('build/js'))
-  gulp.src('css/style.css')
-    .pipe(uglifycss())
-    .pipe(rename({
-        basename: "style",
-        suffix: ".min",
-        extname: ".css"
-    }))
-    .pipe(gulp.dest('build/css'))
-  gulp.src('index.html')
+gulp.task('copyjquery', function() {
+  gulp.src('js/jquery.min.js')
     .pipe(copy())
-    .pipe(gulp.dest('build'))
-  gulp.src('img/background.jpg')
-    .pipe(copy())
-    .pipe(gulp.dest('build/img'))
-    gulp.src('js/jquery.min.js')
-    .pipe(copy())
-    .pipe(gulp.dest('build/js'))
-}); 
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('inj',function () {
+  var target = gulp.src('./build/index.html')
+  var sources = gulp.src(['./build/**/*.js', './build/**/*.css'], {read:false});
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./build'));
+})
+
+gulp.task('build', ['minifyjs','minifycss','copyimg','copyhtml','copyjquery','inj']); 
